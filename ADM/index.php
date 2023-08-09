636434bd3025c588b11e8f3ba5a6d545347aa6ed
Luísa Vacilotto <luvacilotto@gmail.com>
2023-08-09 15:48:32 -0300

    Aula "Como usar URL amigável"

---
+++ ADM/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Celke - Administrativo</title>
+    </head>
+    <body>
+        <?php 
+            require './core/ConfigController.php';
+            $home = new ConfigController();
+        ?>
+    </body>
+</html>
